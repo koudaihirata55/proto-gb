b7f50cb63fa1259cfeaa987c64701e9a4dc7e292
koudaihirata55 <koudaihirata55@gmail.com>
2026-07-02 15:39:25 +0900

    First upload

---
+++ css/style.css
@@ -0,0 +1,41 @@
+body{
+    margin:0;
+    background:#202124;
+    color:white;
+    font-family:Arial,sans-serif;
+}
+
+header{
+    background:#111;
+    padding:40px;
+    text-align:center;
+}
+
+.hero{
+    text-align:center;
+    padding:60px;
+}
+
+.playButton{
+    display:inline-block;
+    margin-top:25px;
+    padding:18px 40px;
+    background:#00b050;
+    color:white;
+    text-decoration:none;
+    border-radius:10px;
+    font-size:24px;
+}
+
+section{
+    max-width:900px;
+    margin:auto;
+    padding:30px;
+}
+
+footer{
+    text-align:center;
+    padding:40px;
+    background:#111;
+    margin-top:40px;
+}
---
+++ js/player.js
@@ -0,0 +1,1 @@
+console.log("PROTO Player");
